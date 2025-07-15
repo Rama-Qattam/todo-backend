0b70b4c99b5abf4adc894bca12c693151613de03
Rama-Qattam <rama.qattam@sitech.me>
2025-07-15 11:20:28 +0300

    feat: add global validation pipe with transformation and whitelisting options

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './common/filters/http-exception.filter';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalFilters(new AllExceptionsFilter());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      stopAtFirstError: false,
+    }),
+  );
 
   const port = process.env.PORT ?? 3000;
   await app.listen(port);
